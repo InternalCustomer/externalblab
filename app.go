1b46d1db575ab3bbb6c4e41f1483e94f15c1d9a9
Fabrizia Binetti <fabriziabinetti@fabrizias-mbp.rot.it.ibm.com>
2015-11-25 11:50:28 +0100

    colpo grosso

--- app.go
+++ app.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"html/template"
 	//for extracting service credentials from VCAP_SERVICES
-	//"github.com/cloudfoundry-community/go-cfenv"
+	"github.com/cloudfoundry-community/go-cfenv"
 )
 
 const (
@@ -23,6 +23,7 @@ func helloworld(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	
 	var port string
 	if port = os.Getenv("PORT"); len(port) == 0 {
 		port = DEFAULT_PORT
@@ -30,7 +31,7 @@ func main() {
 
 	http.HandleFunc("/", helloworld)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	
+
 	log.Printf("Starting app on port %+v\n", port)
 	http.ListenAndServe(":"+port, nil)
 }
