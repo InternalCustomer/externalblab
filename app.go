8a6c0d3e0ce5a4e9eaa92709174366b184eb23da
Fabrizia Binetti <fabriziabinetti@fabrizias-mbp.rot.it.ibm.com>
2015-11-26 10:10:04 +0100

    backup 1

--- app.go
+++ app.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -8,6 +10,26 @@ import (
 	//for extracting service credentials from VCAP_SERVICES
 	"github.com/cloudfoundry-community/go-cfenv"
 )
+
+type post struct {
+
+	Id string `json:"_id"`
+	Rev string `json:"_rev"`
+	Title string
+	Author string
+	Data string
+	Text string
+	Category []string
+}
+
+type cloudant_data struct {
+	
+	Total_rows int //int
+	Offset int //int
+	Rows []struct {
+        Id string
+    }
+}
 
 const (
 	DEFAULT_PORT = "8080"
@@ -42,12 +64,56 @@ func main() {
   }
 
   creds := cloudantServices[0].Credentials
+  username := creds["username"]
+	password := creds["password"]
 
-	log.Printf("username: \n%+v\n", creds["username"])
-	log.Printf("password: \n%+v\n", creds["password"])
+	//ACCESS TO CLOUDANT
+	//GET https://$USERNAME:$PASSWORD@$USERNAME.cloudant.com
+	basicUrl := "https://" + username.(string) + ":" + password.(string) + "@" + username.(string) + ".cloudant.com"
 
-  //db = mysql.New("tcp", "", creds["hostname"]+":"+creds["port"], creds["username"], creds["password"], creds["name"])
-	
+	resp, err := http.Get(basicUrl)
+
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		//fmt.Printf("%s", err)
+		log.Printf("Err body: %s\n", err)
+		os.Exit(1)
+	}
+	log.Printf("Body: %s\n", string(body))
+	//fmt.Printf("%s\n", string(contents))
+
+
+	//LIST ALL DOCS IN blab_data
+	//GET https://$USERNAME:$PASSWORD@$USERNAME.cloudant.com/blab_data/_all_docs
+	allDocsUrl := basicUrl + "/blab_data/_all_docs"
+
+	resp, err = http.Get(allDocsUrl)
+
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+
+	//encoding JSON response - post info
+	var p post
+	err = json.Unmarshal(body, &p)
+	if err != nil {
+		log.Printf("Err body: %s\n", err)
+		os.Exit(1)
+	}
+	log.Printf("Body: %s\n", string(body))
+	log.Printf("Title: %+v\n",p)
+	log.Printf("Title: %s\n",p.Title)
+	log.Printf("author: %s\n",p.Author)
+
 	var port string
 	if port = os.Getenv("PORT"); len(port) == 0 {
 		port = DEFAULT_PORT
